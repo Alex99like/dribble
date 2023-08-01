c15e70bc12f84a0b8fc722c4e7132a95ce5aa8d6
Alexander <aleksievisa@gmail.com>
2023-08-01 14:02:52 +0300

    init

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './global.css'
+
+export const metadata = {
+  title: 'Flexibble',
+  description: 'Showcase and discover reparable developer projects',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        Navbar
+        <main>
+          {children}
+        </main>
+        Footer
+      </body>
+    </html>
+  )
+}
